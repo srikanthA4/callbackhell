c0adf112bf66f879a9318a1302a3dc2344ff098d
srikanthA4 <94035252+srikanthA4@users.noreply.github.com>
2021-12-22 00:42:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var div=document.getElementById('timer')
+ 
+setTimeout(()=>{
+     div.innerHTML=10
+       setTimeout(()=>{
+           div.innerHTML=9
+           setTimeout(()=>{
+              div.innerHTML=8
+               setTimeout(()=>{
+                   div.innerHTML=7
+                   setTimeout(()=>{
+                      div.innerHTML=6
+                       setTimeout(()=>{
+                          div.innerHTML=5
+                           setTimeout(()=>{
+                             div.innerHTML=4
+                               setTimeout(()=>{
+                                  div.innerHTML=3
+                                   setTimeout(()=>{
+                                      div.innerHTML=2
+                                       setTimeout(()=>{
+                                          div.innerHTML=1
+                                           setTimeout(()=>{
+                                               div.innerHTML=0
+                                               setTimeout(()=>{
+                                                   div.innerHTML='many more happy return of the mahitha my darling bestie';
+                                               },1000)
+                                           },1000)
+                                       },1000)
+                                   },1000)
+                                 
+                               },1000)
+                           },1000)
+                       },1000)
+                   },1000)
+               },1000)
+           },1000)
+       },1000)
+   },1000)
