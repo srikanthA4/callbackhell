5c5704da6512188f73644be78f911cda90947df7
srikanthA4 <94035252+srikanthA4@users.noreply.github.com>
2022-02-21 23:48:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,89 @@
-var div=document.getElementById('timer')
- 
-setTimeout(()=>{
-     div.innerHTML=10
-       setTimeout(()=>{
-           div.innerHTML=9
-           setTimeout(()=>{
-              div.innerHTML=8
-               setTimeout(()=>{
-                   div.innerHTML=7
-                   setTimeout(()=>{
-                      div.innerHTML=6
-                       setTimeout(()=>{
-                          div.innerHTML=5
-                           setTimeout(()=>{
-                             div.innerHTML=4
-                               setTimeout(()=>{
-                                  div.innerHTML=3
-                                   setTimeout(()=>{
-                                      div.innerHTML=2
-                                       setTimeout(()=>{
-                                          div.innerHTML=1
-                                           setTimeout(()=>{
-                                               div.innerHTML=0
-                                               setTimeout(()=>{
-                                                   div.innerHTML=' many more happie  returns of the DAY my black DARLING{shiva} with a lots of love ❤️❤️❤️ baby 🥰🥰🥰🎂🎂🎂 party mamuluga undakudadu 🥃🥃🥃🍷🍷🍷🥂🥂🥂';
-                                               },1000)
-                                           },1000)
-                                       },1000)
-                                   },1000)
-                                 
-                               },1000)
-                           },1000)
-                       },1000)
-                   },1000)
-               },1000)
-           },1000)
-       },1000)
-   },1000)
+function one(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 1;
+    callback();
+  }, 1000);
+};
+function two(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 2;
+    callback();
+  }, 1000);
+};
+function three(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 3;
+    callback();
+  }, 1000);
+};
+function four(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 4;
+    callback();
+  }, 1000);
+};
+function five(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 5;
+    callback();
+  }, 1000);
+};
+
+function six(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 6;
+    callback();
+  }, 1000);
+};
+
+function seven(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 7;
+    callback();
+  }, 1000);
+};
+
+function eight(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 8;
+    callback();
+  }, 1000);
+};
+
+function nine(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 9;
+    callback();
+  }, 1000);
+};
+
+function ten(callback) {
+  setTimeout(() => {
+      document.getElementById("myid").innerHTML = 10;
+    callback();
+  }, 1000);
+};
+
+ten(() => {
+  nine(() => {
+      eight (() => {
+          seven(() => {
+              six(() => {
+                  five(() => {
+                      four(() => {
+                          three(() => {
+                              two(() => {
+                                  one(() => {
+                                      setTimeout(() =>{
+                                          document.getElementById("myid").innerHTML = "Happy Independence Day";
+                                      }, 2000);                                        
+                                  })
+                              })
+                          })
+                      })
+                  })
+              })
+          })
+      })
+  })
+});
